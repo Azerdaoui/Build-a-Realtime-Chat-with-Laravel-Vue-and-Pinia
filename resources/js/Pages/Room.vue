b2cbd259b4863509343ee71ad871d626c5cd843f
Othmane AZERDAOUI <oazerdaoui@gear9.ma>
2024-07-17 16:28:40 +0100

    Setting up & builfing chat textarea

---
+++ resources/js/Pages/Room.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import ChatTextarea from '@/Components/Chat/ChatTextarea.vue'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+defineProps({
+    room: Object
+});
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ room.title }}
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 grid grid-cols-12 gap-6">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-3">
+                    <div class="p-6 text-gray-900">
+                        Users online
+                    </div>
+                </div>
+
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-9">
+                    <div class="p-6 text-gray-900">
+                        <ChatTextarea 
+                            v-on:valid="console.log($event)"
+                            class="w-full" 
+                            placeholder="Aa..." />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
