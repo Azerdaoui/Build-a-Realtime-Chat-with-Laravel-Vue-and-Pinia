5acfcb814727891445001443664af37f76e9b800
Othmane AZERDAOUI <oazerdaoui@gear9.ma>
2024-07-18 16:31:55 +0100

    Using the intersection observer API

--- resources/js/Components/Chat/ChatMessages.vue
+++ resources/js/Components/Chat/ChatMessages.vue
@@ -1,14 +1,29 @@
 <script setup>
-    import {useMessagesStore} from '@/Store/useMessagesStore.js'
+    import { useMessagesStore } from '@/Store/useMessagesStore.js'
+    import { useIntersectionObserver }  from '@vueuse/core'
+    import { ref } from "vue"
 
     const messageStore = useMessagesStore()
+
+    const target = ref(null)
+
+    const { stop } = useIntersectionObserver(
+        target,
+        ([{isIntersecting}], observerElement) => {
+            if( isIntersecting ) {
+                console.log(isIntersecting)
+            }
+        }
+    )
 </script>
 
 <template>
     <div class="h-64 border border-gray-200 rounded-lg px-4 py-3 overflow-y-auto flex flex-col-reverse">
-        <div v-for="message in messageStore.messages" :key="message.id"
-            class="flex space-x-3 mb-3">
-            <img :src="message.user.avatar" class="size-12 rounded-xl">
+
+        <div v-for="message in messageStore.messages" :key="message.id" class="flex space-x-3 mb-3">
+
+            <img :src="message.user.avatar" class="size-12 rounded-xl" alt="">
+
             <div>
                 <div class="flex items-baseline space-x-2">
                     <div class="font-bold">{{ message.user.name }}</div>
@@ -20,5 +35,7 @@
                 </p>
             </div>
         </div>
+
+        <div ref="target" class="translate-y-20"></div>
     </div>
 </template>
